a38d3e84b14bcdd64ddff3d323d55c8b807cc15d
Manoj Prabakar <manojprabakar20022000@gmail.com>
2026-07-30 19:23:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer.jsx";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
